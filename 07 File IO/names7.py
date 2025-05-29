a56dda5daa3115566d5c067c48caffcf4e330591
Rahul C G <rahulchandrang@gmail.com>
2025-05-29 09:29:33 +0530

    Added Regular Expressions

--- 07 File IO/names7.py
+++ 07 File IO/names7.py
@@ -6,5 +6,5 @@ with open("names.txt") as file:
     for line in file:
         names.append(line.rstrip())
 
-for name in sorted(names):
+for name in sorted(names): # To sort descending for name in sorted(names,reverse=True)
     print(f"hello, {name}")
